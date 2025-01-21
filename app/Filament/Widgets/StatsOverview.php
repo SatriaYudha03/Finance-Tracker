65f4616d23afe9bf37b2cbae1e8a781b5af44271
Satria Yudha <satriayuda2017@gmail.com>
2025-01-21 11:00:01 +0800

    add date filter

--- app/Filament/Widgets/StatsOverview.php
+++ app/Filament/Widgets/StatsOverview.php
@@ -2,16 +2,32 @@
 
 namespace App\Filament\Widgets;
 
+use Illuminate\Support\Carbon;
 use App\Models\Transaction;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
+use Filament\Widgets\Concerns\InteractsWithPageFilters;
 
 class StatsOverview extends BaseWidget
 {
+    use InteractsWithPageFilters;
+
     protected function getStats(): array
     {
-        $pemasukan = Transaction::incomes()->get()->sum('amount');
-        $pengeluaran = Transaction::expanses()->get()->sum('amount');
+        $startDate = ! is_null($this->filters['startDate'] ?? null) ?
+            Carbon::parse($this->filters['startDate']) :
+            null;
+
+        $endDate = ! is_null($this->filters['endDate'] ?? null) ?
+            Carbon::parse($this->filters['endDate']) :
+            now();
+
+        $pemasukan = Transaction::incomes()
+                    ->whereBetween('date_transaction', [$startDate, $endDate])
+                    ->sum('amount');
+        $pengeluaran = Transaction::expanses()
+                    ->whereBetween('date_transaction', [$startDate, $endDate])
+                    ->sum('amount');
         return [
             Stat::make('Total Pemasukan', $pemasukan),
             Stat::make('Total Pengeluaran', $pengeluaran),
--- app/Filament/Widgets/WidgetIncomeChart.php
+++ app/Filament/Widgets/WidgetIncomeChart.php
@@ -2,25 +2,36 @@
 
 namespace App\Filament\Widgets;
 
+use Illuminate\Support\Carbon;
 use App\Models\Transaction;
 use Flowframe\Trend\Trend;
 use Flowframe\Trend\TrendValue;
 use Filament\Widgets\ChartWidget;
+use Filament\Widgets\Concerns\InteractsWithPageFilters;
 
 class WidgetIncomeChart extends ChartWidget
 {
     protected static ?string $heading = 'Income';
     protected static string $color = 'success';
+    use InteractsWithPageFilters;
 
     protected function getData(): array
     {
+        $startDate = ! is_null($this->filters['startDate'] ?? null) ?
+            Carbon::parse($this->filters['startDate']) :
+            null;
+
+        $endDate = ! is_null($this->filters['endDate'] ?? null) ?
+            Carbon::parse($this->filters['endDate']) :
+            now();
+
         // Menggunakan Trend::model() dengan nama model Transaction dan menerapkan scope expanses di dalam query
         $data = Trend::query(Transaction::whereHas('category', function ($query) {
             $query->where('is_expanse', false); // Filter berdasarkan kategori pengeluaran
         }))
         ->between(
-            start: now()->startOfYear(),
-            end: now()->endOfYear(),
+            start: $startDate,
+            end: $endDate,
         )
         ->perDay()
         ->sum('amount');
